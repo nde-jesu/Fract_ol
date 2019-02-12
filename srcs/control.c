784cbc8720e71cdbe4875c3cac8e69d5cdec714a
Nicolas DE JESUS GONCALVES <nde-jesu@e1r8p4.42.fr>
2019-02-12 13:55:05 +0100

    Control good

--- srcs/control.c
+++ srcs/control.c
@@ -5,92 +5,73 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nde-jesu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/12 13:51:14 by nde-jesu          #+#    #+#             */
-/*   Updated: 2019/02/12 13:51:17 by nde-jesu         ###   ########.fr       */
+/*   Created: 2019/02/12 13:53:28 by nde-jesu          #+#    #+#             */
+/*   Updated: 2019/02/12 13:54:43 by nde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "mlx_macro_keys.h"
 #include "fractol.h"
 #include <mlx.h>
-#include "mlx_macro_keys.h"
 
-void	change_type_julia(int key, t_fract *fract)
+void	reload(t_fract *fract)
 {
-	fract = new_img(fract);
-	if (key == KEY_PAGE_UP)
-	{
-		++fract->type_julia;
-		if (fract->type_julia == 6)
-			fract->type_julia = 0;
-	}
-	else if (key == KEY_PAGE_DOWN)
-	{
-		--fract->type_julia;
-		if (fract->type_julia == -1)
-			fract->type_julia = 5;
-	}
-	init_params(fract, 1);
-	reload(fract);
+	if (fract->type == 1)
+		mandel(fract);
+	else if (fract->type == 2)
+		julia(fract);
+	else if (fract->type == 3)
+		koch(fract);
+	else if (fract->type == 4)
+		barnsley(fract);
 }
 
-void	change_fract(int key, t_fract *fract)
+int		close(void *param)
 {
-	fract = new_img(fract);	
-	if (key == KEY_LESS_THAN)
-	{
-		--fract->type;
-		if (fract->type == 0)
-			fract->type = 4;
-	}
-	if (key == KEY_MORE_THAN)
-	{
-		++fract->type;
-		if (fract->type == 5)
-			fract->type = 1;
-	}
-	init_params(fract, 1);
-	reload(fract);
+	free(param);
+	exit(0);
+	return (0);
 }
 
-void	zoom(int key, t_fract *fract)
+int		user_command(int key, void *param)
 {
-	fract = new_img(fract);
-	if (key == KEY_PAD_ADD || key == KEY_EQUAL || key == MOUSE_SCROLL_UP)
-	{
-		fract->min.x = (fract->mouse.act_x / fract->zoom + fract->min.x)\
-					   - (fract->mouse.act_x / (fract->zoom * 1.3));
-		fract->min.y = (fract->mouse.act_y / fract->zoom + fract->min.y)\
-					   - (fract->mouse.act_y / (fract->zoom * 1.3));
-		fract->zoom *= 1.1;
-	}
-	else if (key == KEY_PAD_SUB || key == KEY_MINUS || key == MOUSE_SCROLL_DOWN)
-	{
-		fract->min.x = (fract->mouse.act_x / fract->zoom + fract->min.x)\
-					   - (fract->mouse.act_x / (fract->zoom / 1.3));
-		fract->min.y = (fract->mouse.act_y / fract->zoom + fract->min.y)\
-					   - (fract->mouse.act_y / (fract->zoom / 1.3));
-		fract->zoom /= 1.1;
-	}
-	reload(fract);
+	t_fract*fract;
+
+	fract = (t_fract*)param;
+	if (key == KEY_ESCAPE)
+		close(param);
+	if (key == KEY_M)
+		fract->mouse.toggle_mouse *= -1;
+	if (key == KEY_PAD_ADD || key == KEY_PAD_SUB || key == KEY_MINUS \
+			|| key == KEY_EQUAL)
+		zoom(key, fract);
+	if (key == KEY_SPACEBAR)
+		space(fract);
+	else if (key == KEY_LEFT || key == KEY_RIGHT || key == KEY_DOWN \
+			|| key == KEY_UP)
+		translation(key, fract);
+	else if (key == KEY_LESS_THAN || key == KEY_MORE_THAN)
+		change_fract(key, fract);
+	else if (key == KEY_PAGE_UP || key == KEY_PAGE_DOWN)
+		change_type_julia(key, fract);
+	return (0);
 }
 
-void	space(t_fract *fract)
+void	get_ctrl(t_fract *fract)
 {
-	fract = new_img(fract);
-	fract->toggle_menu *= -1;
-	reload(fract);
+	mlx_hook(fract->mlx->win, 2, 0, user_command, fract);
+	mlx_hook(fract->mlx->win, 17, 0, close, fract);
+	mlx_hook(fract->mlx->win, 4, 0, press, fract);
+	mlx_hook(fract->mlx->win, 6, 0, move, fract);
 }
 
-void	translation(int key, t_fract *	fract)
+t_fract	*new_img(t_fract *fract)
 {
-	fract = new_img(fract);
-	if (key == KEY_LEFT)
-		fract->min.x += 30 / fract->zoom;
-	if (key == KEY_RIGHT)
-		fract->min.x -= 30 / fract->zoom;
-	if (key == KEY_UP)
-		fract->min.y += 30 / fract->zoom;
-	if (key == KEY_DOWN)
-		fract->min.y -= 30 / fract->zoom;
-	reload(fract);
+	mlx_destroy_image(	fract->mlx->ptr, fract->mlx->img->ptr);
+	fract->mlx->img->ptr = mlx_new_image(fract->mlx->ptr, WIDTH, HEIGHT);
+	fract->mlx->img->data = mlx_get_data_addr(fract->mlx->img->ptr,
+			&(fract->mlx->img->bpp), &(fract->mlx->img->size_l),
+			&(fract->mlx->img->endian));
+	return (fract);
 }
